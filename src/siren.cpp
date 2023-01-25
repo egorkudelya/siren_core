9f5a31f712be1e7e9e98317a5b09413ec77d35e2
egorkudelya <egor.kudelya@gmail.com>
2023-01-25 11:58:25 +0200

    SRN-10: Add core API

---
+++ src/siren.cpp
@@ -0,0 +1,44 @@
+#include "siren.h"
+
+namespace siren
+{
+    SirenEngine* SirenEngine::s_instance = nullptr;
+
+    SirenEngine::SirenEngine(siren::EngineSpecification&& engine_specification)
+        : m_specification(std::move(engine_specification))
+    {
+        s_instance = this;
+    }
+
+    EngineReturnType SirenEngine::make_fingerprint(const std::string& track_path) const
+    {
+        const unsigned int target_sampling_rate = m_specification.engine_params.target_sampling_rate;
+        const unsigned int target_channel_count = m_specification.engine_params.target_channel_count;
+        const size_t target_window_size = m_specification.engine_params.target_window_size;
+        const size_t target_net_size = m_specification.engine_params.target_net_size;
+        const float target_peak_threshold = m_specification.engine_params.target_peak_threshold;
+        const size_t min_peak_count = m_specification.engine_params.min_peak_count;
+        siren::WindowFunction target_window_function = m_specification.engine_params.target_window_function;
+
+        EngineReturnType return_obj;
+
+        std::unique_ptr<siren::KissFFT> fft = std::make_unique<siren::KissFFT>(target_window_function, target_window_size);
+        std::unique_ptr<siren::audio::PCM> audio = std::make_unique<siren::audio::PCM>(track_path, target_channel_count, target_sampling_rate);
+
+        if (!audio->config_decoder())
+        {
+            return_obj.return_code = EngineStatus::TargetFileDoesNotExist;
+            return return_obj;
+        }
+
+        siren::PeakSpectrogram spectrogram(std::move(audio), std::move(fft), target_peak_threshold);
+
+        siren::Fingerprint fingerprint;
+        EngineStatus code = fingerprint.make_fingerprint(std::move(spectrogram), target_net_size, min_peak_count);
+
+        return_obj.return_code = code;
+        return_obj.fingerprint = std::move(fingerprint);
+
+        return return_obj;
+    }
+}// namespace siren
---
+++ src/siren.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <iostream>
+#include <memory>
+
+#include "decoder/pcm.h"
+#include "fft/fft.h"
+#include "entities/fingerprint.h"
+#include "entities/spectrogram.h"
+
+namespace siren
+{
+
+    struct EngineParameters {
+        /**
+        * default target parameters can by overridden by client in CreateEngine
+        */
+
+        enum class HashingPattern
+        {
+            Sequential,
+            Triangular,
+            Rectangular
+        };
+
+        unsigned int    target_sampling_rate = 11025;
+        unsigned int    target_channel_count = 1;
+        size_t          target_window_size = 1024;
+        float           target_peak_threshold = 1.65;
+        size_t          min_peak_count = 160;
+        size_t          target_net_size = 80;
+        WindowFunction  target_window_function = WindowFunction::Hanning;
+    };
+
+    struct EngineSpecification {
+        std::string name = "Siren Fingerprinting Engine";
+        EngineParameters engine_params;
+    };
+
+    struct EngineReturnType {
+        Fingerprint<siren::PeakSpectrogram> fingerprint{};
+        EngineStatus return_code;
+    };
+
+    class SirenEngine
+    {
+    public:
+        explicit SirenEngine(siren::EngineSpecification&& engine_specification);
+
+        static SirenEngine& get_instance()
+        {
+            return *s_instance;
+        }
+
+        [[nodiscard]] EngineReturnType make_fingerprint(const std::string& track_path) const;
+
+    private:
+        EngineSpecification m_specification;
+
+    private:
+        static SirenEngine* s_instance;
+    };
+
+    std::unique_ptr<SirenEngine> CreateEngine();
+}// namespace siren
